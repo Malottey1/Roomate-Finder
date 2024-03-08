1fbc7beb3269df5536ced9f8a94b1416088d147d
P-Asare <pinopalal11@gmail.com>
2024-03-08 21:22:19 +0000

    create database query

--- actions/login_user_action.php
+++ actions/login_user_action.php
@@ -1,4 +1,29 @@
 <?php
-    echo "working";
+    include("../settings/connection.php");
+
+    if (isset($_POST['login-btn'])){
+        
+        $email = $_POST['username'];
+        $passwrd = $_POST['password'];
+
+        $sql = "SELECT * FROM users WHERE email = ?";
+        
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+
+        // get the rows from the query execution
+        $result = mysqli_stmt_get_result($stmt);
+
+        if(mysqli_num_rows($result) == 0){
+            echo "<script>alert('Unsuccessful query.');</script>";
+            exit();
+        }
+        
+        $row = mysqli_fetch_assoc($result);
+
+
+
+    }
 
 ?>
