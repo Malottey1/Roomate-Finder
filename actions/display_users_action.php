0f76603eb0f87ad278026e9386a82f357ea70e0a
P-Asare <pinopalal11@gmail.com>
2024-03-10 11:02:37 +0000

    create and call functions to diplay hostel details of user

--- actions/display_users_action.php
+++ actions/display_users_action.php
@@ -21,8 +21,36 @@
         }
     }
 
+    // Get the hostel details of a particular user
+    function get_user_hostel($id){
+        global $conn;
+
+        $sql = "SELECT * FROM room_listings WHERE listing_id = ?";
+
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $id);
+
+        $result = mysqli_stmt_execute($stmt);
+
+        if (!$result){
+            die("Unsuccessful query: ".mysqli_error($conn));
+        }
+        else {
+            $result_set = mysqli_stmt_get_result($stmt);
+
+            if (mysqli_num_rows($result_set) > 0){
+                $hostel = mysqli_fetch_all($result_set, MYSQLI_ASSOC);
+                return $hostel;
+            }
+        }
+    }
+
     function display_each_user($users){
+
         foreach ($users as $user){
+
+            $hostel = get_user_hostel($user['listing_id']);
+
             echo "<div class='card'>";
             echo "<div><img src='../assets/images/Rectangle 38.jpg' alt='card image'></div>";
             echo '<div class="lower">';
@@ -31,8 +59,14 @@
             echo '<div style="margin-right: 10px;" class="material-symbols-outlined send-btn">send</div>';
             echo '<div class="material-symbols-outlined">thumb_down</div>';
             echo '</div></div>';
-            echo '<div><p class="location">Dufiex Annex, GHS 7000</p></div>';
+            
+            // display hostel details
+            echo "<div>";
+            foreach ($hostel as $h) {
+                echo "<p class='location'>".$h['hostel_name'].", GHS".$h['hostel_cost']."</p>";
+            }
+            echo "</div>";
             echo '</div>';
-        }
+            }
     }
 ?>
