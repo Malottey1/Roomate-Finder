44e82b9babd7bed6a7517fe80c983f8794986428
P-Asare <pinopalal11@gmail.com>
2024-03-11 02:22:34 +0000

    create filter output when all inputs are filled

--- actions/filter_listings_action.php
+++ actions/filter_listings_action.php
@@ -1,32 +1,58 @@
 <?php
 
     include("../settings/connection.php");
+    include("../actions/display_users_action.php");
 
 
-    echo "<p>Works</p>";
+    $gender = $_POST['gender'];
+    $age = date("Y") - $_POST['age'];
+    $hostel = $_POST['hostel'];
+    $ethnicity = $_POST['ethnicity'];
 
-    // $gender = $_POST['gender'];
-    // $age = date("Y") - $_POST['age'];
-    // $hostel = $_POST['hostel'];
-    // $ethnicity = $_POST['ethnicity'];
+    $sql = "SELECT * FROM users WHERE gender = ? AND ethnicity = ? AND listing_id = ? AND Year(dob) = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "iiii", $gender, $ethnicity, $hostel, $age);
 
-    // $sql = "SELECT * FROM users WHERE gender = ? AND ethnicity = ? AND listing_id = ? AND Year(dob) = ?";
-    // $stmt = mysqli_prepare($conn, $sql);
-    // mysqli_stmt_bind_param($stmt, "iiii", $gender, $ethnicity, $hostel, $age);
+    $result = mysqli_stmt_execute($stmt);
 
-    // $result = mysqli_stmt_execute($stmt);
+    if (!$result){
+        die("Unsuccessful query: ".mysqli_error($conn));
+    }
+    else {
+        $result_set = mysqli_stmt_get_result($stmt);
 
-    // if (!$result){
-    //     die("Unsuccessful query: ".mysqli_error($conn));
-    // }
-    // else {
-    //     $result_set = mysqli_stmt_get_result($stmt);
+        if (mysqli_num_rows($result_set) > 0){
+            $users = mysqli_fetch_all($result_set, MYSQLI_ASSOC);
+        }
 
-    //     if (mysqli_num_rows($result_set) > 0){
-    //         $users = mysqli_fetch_all($result_set, MYSQLI_ASSOC);
-    //     }
+    }
 
-    // }
+    $html = "";
+
+    foreach ($users as $user){
+
+        $hostel = get_user_hostel($user['listing_id']);
+
+        $html .= "<div class='card'>";
+        $html .= "<div><img src='../assets/images/Rectangle 38.jpg' alt='card image'></div>";
+        $html .= '<div class="lower">';
+        $html .= '<div><p class="name">'.$user["first_name"].' '.$user["last_name"].'</p></div>';
+        $html .= '<div class="icons">';
+        $html .= '<div style="margin-right: 10px;" class="material-symbols-outlined send-btn">send</div>';
+        $html .= '<div class="material-symbols-outlined">thumb_down</div>';
+        $html .= '</div></div>';
+        
+        // display hostel details
+        $html .= "<div>";
+        foreach ($hostel as $h) {
+            $html .= "<p class='location'>".$h['hostel_name'].", GHS".$h['hostel_cost']."</p>";
+        }
+        $html .= "</div>";
+        $html .= '</div>';
+    }
+
+    echo $html;
+
 
 
 ?>
