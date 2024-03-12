961c630d294211476ab0ed533c778adf967ce644
P-Asare <pinopalal11@gmail.com>
2024-03-12 15:31:54 +0000

    add extra query to insert profile details

--- actions/register_user_action.php
+++ actions/register_user_action.php
@@ -13,8 +13,9 @@
         $ethnicity = $_POST['ethnicity'];
         $dob = $_POST['dateOfBirth'];
         $hostel = $_POST['hostel'];
-        // $bio = $_POST['bio'];
-        // $fbook = $_POST['socialMedia'];
+        
+        $bio = $_POST['bio'];
+        $fbook = $_POST['socialMedia'];
 
         if ($passwrd == $c_psswrd){
             $hashed_passwrd = password_hash($passwrd, PASSWORD_DEFAULT);
@@ -29,7 +30,29 @@
         mysqli_stmt_bind_param($stmt, "ssssisis", $email, $hashed_passwrd, $fname, $lname, $gender, $dob, $ethnicity, $hostel);
         $result = mysqli_stmt_execute($stmt);
 
-        if ($result){
+        // Get the user id of the user
+        $sql2 = "SELECT * FROM users WHERE email = ?";
+        $stmt3 = mysqli_prepare($conn, $sql2);
+        mysqli_stmt_bind_param($stmt3, "s", $email);
+        mysqli_stmt_execute($stmt3);
+        $res = mysqli_stmt_get_result($stmt3);
+
+        if($res){
+            if(mysqli_num_rows($res) > 0){
+                $user = mysqli_fetch_all($res, MYSQLI_ASSOC);
+            }
+        }
+
+        // gets the id of a single user
+        foreach($user as $u){
+            $sql3 = "INSERT INTO profile (user_id, bio, facebook) VALUES (?, ?, ?)";
+            $stmt2 = mysqli_prepare($conn, $sql3);
+            mysqli_stmt_bind_param($stmt2, "iss", $u['user_id'], $bio, $fbook);
+            $result2 = mysqli_stmt_execute($stmt2);
+        }
+        
+        // If all querries worked
+        if ($result && $result2){
             header("Location: ../view/dashboard-postLogin.php");
             exit();
         }
