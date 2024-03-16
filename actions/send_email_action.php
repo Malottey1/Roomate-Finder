aa5e0fa44c027aa8f025622bcc39250247d662da
P-Asare <pinopalal11@gmail.com>
2024-03-16 16:45:44 +0000

    complete send email action with mailto

--- actions/send_email_action.php
+++ actions/send_email_action.php
@@ -18,7 +18,18 @@
             }
         }
 
+        // define email details
         $to = $user[0]['email'];
+        $subject = "Discussion+on+rooming+together";
+        $body = "Hi, My name is".$user[0]['first_name'].". I am interested in rooming with your and would like to discuss more with you. Kindly reach out if you're interested too.";
+
+        // to prevent unnecessary spaces in mail
+        $body = str_replace(' ', '+', $body);
+
+        // Create mailto and open mail to contact user
+        $mail_to_link = "mailto:$to?subject=".urlencode($subject)."&body=".urlencode($body);
+        header("Location: $mail_to_link");
+        exit;
 
 
     }
