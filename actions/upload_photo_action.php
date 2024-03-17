2359fe8b363faa379a1e1884914401f3c57cca4d
P-Asare <pinopalal11@gmail.com>
2024-03-17 00:55:36 +0000

    sql code to update database and place image in folder

--- actions/upload_photo_action.php
+++ actions/upload_photo_action.php
@@ -1,7 +1,32 @@
 <?php
 
+session_start();
+
+include("../settings/connection.php");
+
 if(isset($_FILES['file'])){
-    echo "working";
+    
+    $image_directory = "../images/";
+    $filename = $_SESSION['user-id']."_".$_FILES['file']['name'];
+    $user = $_SESSION['user-id'];
+
+    // Find user and upload profile photo
+    $sql = "UPDATE profile SET photo_name = ? WHERE user_id = ?";
+
+    $stmt = mysqli_prepare($conn, $sql);
+
+    // mysqli_stmt_bind_param($stmt, "isss", $user, $filename, $bio, $fb);
+    mysqli_stmt_bind_param($stmt, "si", $filename, $user);
+    $result = mysqli_stmt_execute($stmt);
+
+    if(!$result){
+        echo "Error updating record: ".mysqli_error($conn);
+        exit;
+    }
+
+    // Place file in correct folder
+    move_uploaded_file($_FILES['file']['tmp_name'], $image_directory.$filename);
+
 }
 
 ?>
