8d72956a6857e08dc0bcccbc70a8b072d1aec25c
P-Asare <pinopalal11@gmail.com>
2024-03-17 10:05:05 +0000

    route click to roommate profile

--- actions/display_roommate_action.php
+++ actions/display_roommate_action.php
@@ -4,6 +4,20 @@
     include("../functions/show_profile_fxn.php");
     include("../functions/get_profile_info_fxn.php");
 
-    echo "Working: ".$_GET['uid'];
+    $uid = $_GET['uid'];
+
+    $profile = get_profile_info($uid);
+
+    if($profile != null){
+        $dislike_comments = json_decode($profile[0]['COMMENT']);
+        $preference_comments = json_decode($profile[0]['comment']);
+    
+        $picture = get_profile_photo($uid);
+
+        header("Location: ../view/roommate-profile.php");
+        exit;
+    }
+
+    header("Location: ../view/roommate-listings.php");
 
 ?>
