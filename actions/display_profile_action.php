c48f9a5a6bc41d8a45babee9b9a33b7e79176c7e
P-Asare <pinopalal11@gmail.com>
2024-03-17 09:31:29 +0000

    refactor display profile action with new function

--- actions/display_profile_action.php
+++ actions/display_profile_action.php
@@ -2,29 +2,14 @@
 
     include("../settings/connection.php");
     include("../functions/show_profile_fxn.php");
+    include("../functions/get_profile_info_fxn.php");
 
     session_start();
 
     $uid =  $_SESSION['user-id'];
     $profile = null;
 
-    $sql = "SELECT  users.user_id, first_name, last_name, bio, preferences.comment, dislikes.COMMENT
-    FROM users
-    JOIN profile ON users.user_id = profile.user_id 
-    JOIN dislikes ON users.user_id = dislikes.user_id 
-    JOIN preferences ON users.user_id = preferences.user_id 
-    WHERE users.user_id = $uid";
-
-    $result = mysqli_query($conn, $sql);
-
-    if(!$result){
-        die("Unsuccessful query: ".mysqli_error($conn));
-    }
-    else{
-        if(mysqli_num_rows($result) > 0){
-            $profile = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        }
-    }
+    $profile = get_profile_info($uid);
 
     if($profile != null){
         $dislike_comments = json_decode($profile[0]['COMMENT']);
